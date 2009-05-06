be25e291e4c034735ad53bed91d3bea78b4b24b5
jcap <git@jcap.me>
2009-05-06 00:04:39 -0400

    migrate from AppConfig to JSON

--- lib/JSAN/Config.pm
+++ lib/JSAN/Config.pm
@@ -2,38 +2,30 @@ package JSAN::Config;
 use strict;
 use warnings;
 
-use AppConfig;
+use JSON;
 
 our $CONFIG;
 
 sub new {
     return $CONFIG if $CONFIG;
-    my $conf = AppConfig->new;
-    $conf->define($_) for map {("live_$_", "dev_$_")} qw[
-        db=s
-        site=s
-        jause=s
-        dbauth=s
-    ];
-    $conf->define("file|filelist|f=s@");
-    $conf->file(
-          $ENV{JSAN_DEV}
-        ? "/var/www/dev.openjsan.org/etc/jsan.cfg"
-        :   -e "/var/www/master.openjsan.org/etc/jsan.cfg"
-          ? "/var/www/master.openjsan.org/etc/jsan.cfg"
-          : "/var/www/dev.openjsan.org/etc/jsan.cfg"
-    );
+
+    my $conf_filename = $ENV{JSAN_SITE_PREFIX} || '/var/www/';
+    $conf_filename   .= $ENV{JSAN_SITE};
+    $conf_filename   .= '/etc/jsan.cfg';
+
+    open my $conf_file, "<", $conf_filename or die "Couldn't open file: $!"; 
+    my $json = join("", <$conf_file>); 
+    close $conf_file;
+  
     return $CONFIG = bless {
-        conf   => $conf,
-        prefix => ($ENV{JSAN_DEV} ? "dev" : "live"),
+        conf   => from_json($json),
+        prefix => $ENV{JSAN_SITE},
     }, shift;
 }
 
 sub get {
     my ($self, $var) = @_;
-    my $pre = $self->{prefix};
-    $var = "${pre}_$var";
-    return $self->{conf}->$var;
+    return $self->{conf}{site}{$self->{prefix}}{$var};
 }
 
 sub dbauth {
