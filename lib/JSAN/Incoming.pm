5102298277a4fb7ec4b851c09bcec55ab9f21338
SamuraiJack <nickolay8@gmail.com>
2009-08-06 16:52:48 +0400

    - implemented support for already generated 'html' documentation, in case when generation from 'pod' returned empty result

--- lib/JSAN/Incoming.pm
+++ lib/JSAN/Incoming.pm
@@ -440,9 +440,40 @@ sub _parse_inline_pod {
     $self->pod_parser->bare_output(1);
     $self->pod_parser->parse_file($js);
     
-    return $self->_parse_external_pod($js) unless $self->js_libs->{$js}->{html};
-    $self->js_libs->{$js}->{doc_file} = $js;    
+    $self->js_libs->{$js}->{doc_file} = $js;
+    
+    $self->_parse_external_pod($js)         unless $self->js_libs->{$js}->{html};
+    $self->_copy_from_existing_html($js)    unless $self->js_libs->{$js}->{html};
 }
+
+
+sub _copy_from_existing_html {
+    my ($self, $js) = @_;
+    
+    my $srcdist = $self->src_dist;
+    
+    my ($lib) = $js =~ m[$srcdist/lib/(.+)];
+    $lib =~ s/\.js/\.html/;
+    
+    my $html_file = "$srcdist/doc/html/$lib"
+    return unless -e $html_file;
+    
+    my $html;
+    
+    #slurping
+    {
+        local($/) ;
+        open(my $fh, $html_file) or die "Cannot open $html_file: $!\n";
+        $html = < $fh >;
+        close $fh;
+    }
+    
+    if ($html) {
+        $self->js_libs->{$js}->{html} = $html;
+        $self->js_libs->{$js}->{doc_file} = $html_file;
+    }
+}
+
 
 sub _parse_external_pod {
     my ($self, $js) = @_;
